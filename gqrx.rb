2c4b928108647949a76fd4c04bae6350aaa2f22d
Charles H. Leggett <charles@solarturtle.net>
2014-11-01 11:21:54 -0400

    This initial commit does NOT work yet. It fails a gqrx qmake dependancy check for gnuradio-audio.

---
+++ gqrx.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class Gqrx < Formula
+  homepage "http://gqrx.dk"
+  url "https://github.com/csete/gqrx/archive/v2.3.1.tar.gz"
+  sha1 "d24595601c79c894601eaa3cc36d9838134616fe"
+
+  depends_on 'gnuradio'
+  depends_on 'qt'
+
+  def install
+    args = "PREFIX=#{prefix}"
+    mkdir "build" do
+      system "qmake",  *args, ".."
+      system "make"
+    end
+    Dir.glob("build/*.app") { |app| mv app, prefix }
+  end
+
+  test do
+    system "false"
+  end
+end
